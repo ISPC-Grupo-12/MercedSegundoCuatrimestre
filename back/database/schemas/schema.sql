fc13a71b2d057af6cb87f6a2dcb2462b723297e2
DarkSei112 <leyco4313@gmail.com>
2025-09-18 12:41:34 -0300

    Convesión
    Actualización de SQL a SQLite (ejem: varchar=text)

--- back/database/schemas/schema.sql
+++ back/database/schemas/schema.sql
@@ -1,60 +1,56 @@
-create database Merced;
-use merced; 
-
-
 create table Usuario(
-id_usuario INT primary key auto_increment,
-nombre varchar(100) not null,
-apellido varchar(100) not null,
-mail varchar(150) not null unique,
-contrasena varchar(255) not null,
-rol varchar(50) not null,
-dni varchar(50) not null unique
+id_usuario INTEGER primary key AUTOINCREMENT,
+nombre text not null,
+apellido text not null,
+mail text not null unique,
+contrasena text not null,
+rol text not null,
+dni text not null unique
 );
 
 create table Producto(
-id_producto INT primary key auto_increment,
-nombre varchar(200) not null,
-descripcion varchar(1500) not null,
-precio decimal(12,2) not null check (precio>0),
-stock int not null check (stock >0),
-categoria varchar(80) not null
+id_producto INTEGER primary key AUTOINCREMENT,
+nombre text not null,
+descripcion text not null,
+precio real not null check (precio>0),
+stock integer not null check (stock >0),
+categoria text not null
 );
 
 create table Carrito(
-id_carrito int primary key auto_increment,
-id_usuario int not null unique,
+id_carrito integer primary key AUTOINCREMENT,
+id_usuario integer not null unique,
 foreign key (id_usuario) references Usuario(id_usuario)
         on delete cascade 
 );
 
 create table Detalle_carrito(
-id_carrito int not null,
-id_producto int not null,
-cantidad int not null check (cantidad > 0),
-precio_unitario decimal(12,2) not null check (precio_unitario > 0),
-subtotal decimal(12,2) generated always as (precio_unitario * cantidad) stored,
+id_carrito integer not null,
+id_producto integer not null,
+cantidad integer not null check (cantidad > 0),
+precio_unitario real not null check (precio_unitario > 0),
+subtotal real generated always as (precio_unitario * cantidad) stored,
 primary key (id_carrito, id_producto),
 foreign key (id_carrito) references Carrito(id_carrito) on delete cascade,
 foreign key (id_producto) references Producto(id_producto)
 );
 
 create table Pedido(
-id_pedido int primary key auto_increment,
-id_usuario int not null,
+id_pedido integer primary key AUTOINCREMENT,
+id_usuario integer not null,
 fecha date not null,
-estado varchar(150) not null,
-total decimal(12,2) not null check (total>0),
+estado text not null,
+total real not null check (total>0),
 foreign key (id_usuario) references Usuario(id_usuario) on delete cascade 
 
 );
 
 create table Detalle_pedido(
-id_pedido int not null,
-id_producto int not null,
-cantidad int not null check (cantidad >0),
-precio_unitario decimal(12,2) not null check (precio_unitario>0),
-subtotal decimal(12,2) generated always as (precio_unitario* cantidad) stored,
+id_pedido integer not null,
+id_producto integer not null,
+cantidad integer not null check (cantidad >0),
+precio_unitario real not null check (precio_unitario>0),
+subtotal real generated always as (precio_unitario* cantidad) stored,
 primary key (id_pedido, id_producto),
 foreign key (id_pedido) references Pedido(id_pedido) on delete cascade,
 foreign key (id_producto) references Producto(id_producto)
