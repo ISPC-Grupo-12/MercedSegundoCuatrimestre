92db17ce8ec167e611c46a4c802a9cb350e38238
D-LucianoDiLeo <dileoluciano12@gmail.com>
2025-10-14 15:00:17 -0300

    feat(login, registro): Validaciones de login y registro
    Se agregaron validaciones de usuario y contraseña hardcodeadas en login.js
    y verificación de contraseñas con letras y números en registro.js, además de en sus respectivos html.
    Por último se redirige al usuario según el resultado de la validación,

--- front/js/registro.js
+++ front/js/registro.js
@@ -3,15 +3,26 @@ document.getElementById('registrationForm').addEventListener('submit', function 
     event.preventDefault();
 
     const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+    const confirmPassword = document.getElementById('confirmPassword').value.trim();
     const errorMessage = document.getElementById('errorMessage');
+
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/;
+
+    if (!regex.test(password)) {
+    errorMessage.textContent = 'La contraseña debe contener al menos una letra y un número.';
+    errorMessage.style.color = 'wing';
+    return;
+    }
 
     if (password !== confirmPassword) {
     errorMessage.textContent = 'Las contraseñas no coinciden.';
-    } else {
-    errorMessage.textContent = '';
-    alert('¡Registro exitoso!');
-    // Aquí puedes agregar lógica para enviar los datos al servidor
-    this.reset();
+    errorMessage.style.color = 'wing';
+    return;
     }
+
+    errorMessage.textContent = '';
+    alert('Registro exitoso!');
+    this.reset();
+
+    window.location.href = 'login.html';
 });
